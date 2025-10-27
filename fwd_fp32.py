9e302d4857779b487542a7714b00514d11ef5379
luongthecong123 <luongthecong123@gmail.com>
2025-10-27 00:00:31 +0000

    Replace torch.zero with torch.empty

--- fwd_fp32.py
+++ fwd_fp32.py
@@ -198,7 +198,7 @@ class GeneralizedRingAttractorNoGain(nn.Module):
         """
         batch_size = action_signal.shape[0]
         # Pre-allocate intermediate tensors 
-        r_history = torch.empty(batch_size, self.seq_len, self.num_neurons, device='cuda', dtype=torch.float32)
+        # r_history = torch.empty(batch_size, self.seq_len, self.num_neurons, device='cuda', dtype=torch.float32)
          # transposed for better performance
         bump_history = torch.empty(self.seq_len, batch_size, self.num_neurons, device='cuda', dtype=torch.float32)
 
